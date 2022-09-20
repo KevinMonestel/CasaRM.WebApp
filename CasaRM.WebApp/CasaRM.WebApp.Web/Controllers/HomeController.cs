851d31902d2c82c69900753f6100302515e0c276
Kevin Monestel Amador <39996057+KevinMonestel@users.noreply.github.com>
2022-09-20 08:54:27 -0600

    Añadiendo layout para el dashboard.

--- CasaRM.WebApp/CasaRM.WebApp.Web/Controllers/HomeController.cs
+++ CasaRM.WebApp/CasaRM.WebApp.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using CasaRM.WebApp.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,6 +14,7 @@ namespace CasaRM.WebApp.Web.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
             return View();
