171d8d9e8c1e0c58704f2ea589695e54d4d41a0e
Kevin Monestel Amador <39996057+KevinMonestel@users.noreply.github.com>
2022-09-25 14:54:27 -0600

    Area añadida

---
+++ CasaRM.WebApp/CasaRM.WebApp.Web/Areas/Hosts/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using CasaRM.WebApp.Services.Interfaces;
+using CasaRM.WebApp.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CasaRM.WebApp.Web.Areas.Host.Controllers
+{
+    [Area("Hosts")]
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly IHostService _hostService;
+
+        public HomeController(IHostService hostService)
+        {
+            _hostService = hostService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
