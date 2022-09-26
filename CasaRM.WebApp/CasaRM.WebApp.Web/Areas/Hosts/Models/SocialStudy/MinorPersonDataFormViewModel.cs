38f657c853a498cfa36d8214d5fbd80525d18433
Kevin Monestel Amador <39996057+KevinMonestel@users.noreply.github.com>
2022-09-25 18:27:56 -0600

    Se agregan validaciones y lógica inicial.

--- CasaRM.WebApp/CasaRM.WebApp.Web/Areas/Hosts/Models/SocialStudy/MinorPersonDataFormViewModel.cs
+++ CasaRM.WebApp/CasaRM.WebApp.Web/Areas/Hosts/Models/SocialStudy/MinorPersonDataFormViewModel.cs
@@ -1,41 +1,61 @@
-﻿namespace CasaRM.WebApp.Web.Areas.Hosts.Models.SocialStudy
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CasaRM.WebApp.Web.Areas.Hosts.Models.SocialStudy
 {
     public class MinorPersonDataFormViewModel
     {
+        [Required]
         public int Id { get; set; }
 
+        [Required]
         public string FullName { get; set; }
 
+        [Required]
         public string FileNumber { get; set; }
 
+        [Required]
         public DateTime DateOfBirth { get; set; }
 
+        [Required]
         public int Age { get; set; }
 
+        [Required]
         public string Gender { get; set; }
 
+        [Required]
         public string Scholarship { get; set; }
 
+        [Required]
         public string Nacionality { get; set; }
 
+        [Required]
         public string Diagnosis { get; set; }
 
+        [Required]
         public string AttentionService { get; set; }
 
+        [Required]
         public string Treatment { get; set; }
 
+        [Required]
         public string TreatingPhysician { get; set; }
 
+        [Required]
         public string Province { get; set; }
 
+        [Required]
         public string Canton { get; set; }
 
+        [Required]
         public string District { get; set; }
 
+        [Required]
         public string ExactDirection { get; set; }
 
+        [Required]
         public string SocialSecurityType { get; set; }
 
+        [Required]
         public string Observations { get; set; }
     }
 }
--- CasaRM.WebApp/CasaRM.WebApp.Web/Areas/Hosts/Models/SocialStudy/ParentDataFormViewModel.cs
+++ CasaRM.WebApp/CasaRM.WebApp.Web/Areas/Hosts/Models/SocialStudy/ParentDataFormViewModel.cs
@@ -5,36 +5,52 @@ namespace CasaRM.WebApp.Web.Areas.Hosts.Models.SocialStudy
 {
     public class ParentDataFormViewModel
     {
+        [Required]
         public int Id { get; set; }
 
+        [Required]
         public string FullName { get; set; }
 
+        [Required]
         public string Relationship { get; set; }
 
+        [Required]
         public string Identification { get; set; }
 
+        [Required]
         public DateTime DateOfBirth { get; set; }
 
+        [Required]
         public int Age { get; set; }
 
+        [Required]
         public string Gender { get; set; }
 
+        [Required]
         public string MaritalStatus { get; set; }
 
+        [Required]
         public string Nacionality { get; set; }
 
+        [Required]
         public string Scholarship { get; set; }
 
+        [Required]
         public string PhonesNumbers { get; set; }
 
+        [Required]
         public string Ocupation { get; set; }
 
+        [Required]
         public string WorkAddress { get; set; }
 
+        [Required]
         public string Illness { get; set; }
 
+        [Required]
         public string Treatment { get; set; }
 
+        [Required]
         public string Observations { get; set; }
     }
 }
