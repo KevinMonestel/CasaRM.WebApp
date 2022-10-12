b9f04e62d8065750a636c6b99c19cf618f197340
Kevin Monestel Amador <39996057+KevinMonestel@users.noreply.github.com>
2022-10-11 19:20:30 -0600

    Update HistoryTicketFormViewModel.cs

--- CasaRM.WebApp/CasaRM.WebApp.Web/Areas/Hosts/Models/History/HistoryTicketFormViewModel.cs
+++ CasaRM.WebApp/CasaRM.WebApp.Web/Areas/Hosts/Models/History/HistoryTicketFormViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Build.Framework;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CasaRM.WebApp.Web.Areas.Hosts.Models.History
 {
